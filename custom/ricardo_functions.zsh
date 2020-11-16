8981fdee809b6bc0735f6bbeeca9c8bc7e3249a3
Ricardo <GeekRicardozzZ@gmail.com>
2020-11-16 11:48:42 +0800

    rename to ricardo.theme

--- custom/ricardo_functions.zsh
+++ custom/ricardo_functions.zsh
@@ -64,6 +64,7 @@ function rprompt_separator() {
 function prompt_start() {
   local bg_col=%{$BG[104]%}
   
+  local cpu_used=$(ps -A -o %cpu | awk '{s+=$1} END {print s "%"}')
   local ret_status="%(?:%{$fg[green]%} $ICONS[apple_logo] :%{$fg[red]%} $ICONS[apple_logo] )${reset_color}"
   echo "${bg_col}${ret_status}"
 }
@@ -80,11 +81,11 @@ function prompt_dir() {
 
 function prompt_git () {
 
-  local bg_col=$BG[137]
+  local bg_col=$BG[138]
   local fg_col=$FG[209]
 
   local prompt_git_start="%{$fg_col%}$(prompt_separator)"
-  local prompt_git_end="%{$reset_color%}%{$FG[137]%}$(prompt_separator)"
+  local prompt_git_end="%{$reset_color%}%{$FG[138]%}$(prompt_separator)"
   # https://stackoverflow.com/questions/2180270/check-if-current-directory-is-a-git-repository
   if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
     echo "%{$bg_col%}${prompt_git_start} %{$fg[blue]%} $(parse_git_dirty) %{$fg[17]%}$(git_current_branch)$(git_prompt_status)$(git_remote_status)$(git_prompt_remote)${prompt_git_end}"
@@ -122,16 +123,17 @@ function prompt_battery() {
 
   if $(battery_is_charging) ; then
     power_icon="%{$fg[yellow]%}${ICONS[battery_charging]}"
-  elif $(plugged_in) ; then
-    power_icon=''
-    icon=$ICONS[battery_plugged_in]
-    batt_col=$fg[green]
+#  elif $(plugged_in) ; then
+    #power_icon=''
+    #icon=$ICONS[battery_plugged_in]
+#    batt_col=$fg[green]
   fi
   echo "%{$FG[209]%}$(rprompt_separator) %{$BG[209]%} %{$batt_col%}${icon}${power_icon} %{$fg[black]%}${battery_perc}%% %{$reset_color%}"
 }
 
 function prompt_time() {
   local time_icon="%{$fg[cyan]%}${ICONS[time]}"
+  local time_icon="%{$fg[black]%}${ICONS[time]}"
   local the_time="%{$fg[black]%}[%T]"
 
   echo "%{$FG[223]%}$(rprompt_separator) %{$BG[223]%} %{$fg[black]%}${time_icon} ${the_time}"
@@ -146,8 +148,37 @@ function prompt_ruby_rbenv() {
 }
 
 function prompt_weather(){
-    local weatherjson=$(curl -s "https://devapi.qweather.com/v7/weather/now?location=101021700&key=f2359a5269a2481489ee834d76a0cfc9&gzip=n")
-    local temp=$(echo $weatherjson|python3 -c "import sys,json;a=json.load(sys.stdin);print((a['now']['temp']) if a['code']=='200' else '')")
-    local text=$(echo $weatherjson|python3 -c "import sys,json;a=json.load(sys.stdin);print((a['now']['text']) if a['code']=='200' else '')")
-    echo "%{$BG[209]%}%{$FG[104]%}$(rprompt_separator) %{$BG[104]%}%{$fg[white]%} $text $temp℃ %{$reset_color%}"
+    # TODO 时间间隔15min获取天气
+    old_time=$(echo /tmp/temp|awk '{print int($1)}')
+    if [[ ! -f "/tmp/temp" ]] || [[ $(($(date "+%M"| awk '{print int($0)}') % 15)) = 0 ]]; then
+    #if 1 ;then
+        #echo "load weather"
+        localtion="101021200"
+        local weatherjson=$(curl -s "https://devapi.qweather.com/v7/weather/now?location=${localtion}&key=f1389a5269a2481489ee834d76a0cfc9&gzip=n")
+        code=$(echo $weatherjson|python3 -c "import sys,json;a=json.load(sys.stdin);print((a['code']))")
+        if [ ! $code = "200" ]; then
+            local weatherjson=$(curl -s "https://devapi.qweather.com/v7/weather/now?location=${localtion}&key=d437cf0c800d4c2abef5174a0bfe029e&gzip=n")
+        fi
+        local temp=$(echo $weatherjson|python3 -c "import sys,json;a=json.load(sys.stdin);print((a['now']['temp']) if a['code']=='200' else '')")
+        local text=$(echo $weatherjson|python3 -c "import sys,json;a=json.load(sys.stdin);print((a['now']['text']) if a['code']=='200' else '')")
+        echo "$(date '+%M') ${temp}" > /tmp/temp
+        echo $text > /tmp/text
+    else
+        #echo "use old"
+        local temp=$(cat /tmp/temp|awk '{print $2}')
+        local text=$(cat /tmp/text)
+    fi
+    #text=中雨
+    if [ $text = "晴" ];then
+        local icon="%{$FG[222]%}${ICONS[sun]}%{$fg[white]%}"
+    elif [ $text = '多云' ];then
+        local icon="%{$fg[white]%}${ICONS[windy]}%{$fg[white]%}"
+    elif [ $text = '大雨' ];then
+        local icon="%{$FG[17]%}${ICONS[rain]} ${ICONS[rain]} ${ICONS[rain]}%{$fg[white]%}"
+    elif [ $text = '中雨' ];then
+        local icon="%{$FG[17]%}${ICONS[rain]} ${ICONS[rain]}%{$fg[white]%}"
+    elif [[ $text =~ '雨' ]];then #包含
+        local icon="%{$FG[17]%}${ICONS[rain]}%{$fg[white]%}"
+    fi
+    echo "%{$BG[209]%}%{$FG[104]%}$(rprompt_separator) %{$BG[104]%}%{$fg[white]%} $icon $temp℃ %{$reset_color%}"
 }
